6e61c6fd24ac79be6c1f998c6c68d32b4541ecf9
Jackson Santos <jahkisson@gmail.com>
2022-09-26 13:02:03 -0300

    End of Module 10

--- src/test/java/com/jackson/jfood/CuisineRegistrationIT.java
+++ src/test/java/com/jackson/jfood/CuisineRegistrationIT.java
@@ -1,69 +1,117 @@
 package com.jackson.jfood;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 
-import javax.validation.ConstraintViolationException;
-
-import org.junit.jupiter.api.Assertions;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
-import com.jackson.jfood.domain.exception.EntityIsBeingUsedException;
-import com.jackson.jfood.domain.exception.EntityNotFoundException;
 import com.jackson.jfood.domain.model.Cuisine;
-import com.jackson.jfood.domain.service.CuisineRegistrationService;
+import com.jackson.jfood.domain.repository.CuisineRepository;
+import com.jackson.jfood.util.DatabaseCleaner;
+import com.jackson.jfood.util.ResourceUtils;
 
-@SpringBootTest
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CuisineRegistrationIT {
 
+	private static final int NON_EXISTING_CUISINE_ID = 999;
+	@LocalServerPort
+	private int port;
+	private int sizeInitialCuisines;
+	private Cuisine americanCuisine;
+	
 	@Autowired
-	CuisineRegistrationService cuisineRegistration;
+	private DatabaseCleaner databaseCleaner;
 	
-	@Test
-	void shouldAssignId_WhenCuisineRegistrationWithCorrectData() {
-		Cuisine newCuisine = new Cuisine();
-		newCuisine.setName("Chinese");
+	@Autowired
+	private CuisineRepository cuisineRepository;
+	
+	@BeforeEach
+	public void setUp() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cuisines";
 		
-		// action
-		newCuisine = cuisineRegistration.save(newCuisine);
-		
-		// assertions
-		assertThat(newCuisine).isNotNull();
-		assertThat(newCuisine.getId()).isNotNull();
-	}
-
-	@Test
-	void shouldFail_WhenCuisineRegistrationWithEmptyName() {
-		Cuisine newCuisine = new Cuisine();
-		
-		// action
-		ConstraintViolationException expectedError = Assertions.assertThrows(
-				ConstraintViolationException.class, () -> cuisineRegistration.save(newCuisine));
-		
-		
-		assertThat(expectedError).isNotNull();
+		databaseCleaner.clearTables();
+		setUpData();
 	}
 	
 	@Test
-	void shouldFail_WhenRemovingCuisineInUse() {
-		long cuisineId = 1L;
-		
-		// action
-		EntityIsBeingUsedException expectedError = Assertions.assertThrows(
-				EntityIsBeingUsedException.class, () -> cuisineRegistration.remove(cuisineId));
-		
-		assertThat(expectedError).isNotNull();
+	public void mustReturnStatus200_WhenGetCuisines() {
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.statusCode(HttpStatus.OK.value());
 	}
 	
 	@Test
-	void shouldFail_WhenRemovingNonExistingCuisine() {
-		long cuisineId = 99L;
+	public void mustMatchInicialCuisinesSize_WhenGetCuisines() {
+		given()
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", Matchers.hasSize(sizeInitialCuisines))
+			.body("name", Matchers.hasItems(americanCuisine.getName()));
+	}
+	
+	@Test
+	public void mustReturnStatus201_WhenNewCuisineRegistration() {
+		given()
+			.body(ResourceUtils.getContentFromResource("/json/Cuisine/mustReturnStatus201_WhenNewCuisineRegistration.json"))
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}
+	
+	@Test
+	public void mustReturnValidResponseAndStatus_WhenQueryExistingCuisine() {
+		given()
+			.pathParam("cuisineId", americanCuisine.getId())
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{cuisineId}")
+		.then()
+			.statusCode(HttpStatus.OK.value())
+			.body("name", equalTo(americanCuisine.getName()));
+	}
+	
+	@Test
+	public void mustReturnStatus404_WhenQueryNonExistingCuisine() {
+		given()
+			.pathParam("cuisineId", NON_EXISTING_CUISINE_ID)
+			.accept(ContentType.JSON)
+		.when()
+			.get("/{cuisineId}")
+		.then()
+			.statusCode(HttpStatus.NOT_FOUND.value());
+	}
+	
+	private void setUpData() {
+		Cuisine cuisine1 = new Cuisine();
+		cuisine1.setName("Tailandesa");
+		cuisineRepository.save(cuisine1);
+		sizeInitialCuisines++;
 		
-		// action
-		EntityNotFoundException expectedError = Assertions.assertThrows(
-				EntityNotFoundException.class, () -> cuisineRegistration.remove(cuisineId));
-		
-		assertThat(expectedError).isNotNull();
+		americanCuisine = new Cuisine();
+		americanCuisine.setName("American");
+		cuisineRepository.save(americanCuisine);
+		sizeInitialCuisines++;
 	}
 }
